f8d9cdcb9f46473adcf1941936490d75e4d4d637
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-03 18:57:11 -0500

    React challenge

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
+/**
+Challenge: 
+
+Part 1: Create a page of your own using a custom Page component
+
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
+
+Render your list to the page
+
+ */
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-const teams = (
-    <div>
-        <img src="../public/logo512.png" width="40px" alt="react logo" />
-        <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100k stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-    </div>
-)
+function CustomPage() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>Reusability</li>
+                <li>Hirable skills</li>
+            </ol>
+        </div>
+    )
+}
 
-ReactDOM.render(teams, document.getElementById('root'))
+ReactDOM.render(<CustomPage />, document.getElementById('root'))
+
+// Quiz
+
+// 1. Why do we need to `import React from "react"` in our files?
+// React is what defines JSX
+
+// 2. If I were to console.log(page) in index.js, what would show up?
+// A JavaScript object. React elements that describe what React should
+// eventually add to the real DOM for us.
+
+// 3. What's wrong with this code:
+// ```
+// const page = (
+//     <h1>Hello</h1>
+//     <p>This is my website!</p>
+// )
+// ```
+// We need our JSX to be nested under a single parent element
+
+// 4. What does it mean for something to be "declarative" instead of "imperative"?
+// Declarative means I can tell the computer WHAT to do 
+// and expect it to handle the details. Imperative means I need
+// to tell it HOW to do each step.
+
+// 5. What does it mean for something to be "composable"?
+// We have small pieces that we can put together to make something
+// larger/greater than the individual pieces.
