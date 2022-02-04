81e852b01090d0a28a40bb6175be5c45c09cb98a
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-03 22:26:19 -0500

    Add CSS to project

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Main = () => {
     return (
-        <h1>
-            Main
-        </h1>
+        <main className="main--body">
+            <h1 className="main--header">Fun Facts about React</h1>
+                <ul className="main--list">
+                    <li>Was first release in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+        </main>
     );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <h1>
-            Navbar
-        </h1>
+       <nav>
+           <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="react img" className="nav--icon" />
+           <h3 className="nav--logo_text">ReactFacts</h3>
+           <h4 className="nav--title">React Course - Project 1</h4>
+          
+           
+       </nav>
     );
 }
 
